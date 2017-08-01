927ed649b465b0899200b2cab32698e382c41306
noctilucentgames <noctilucentgames@gmail.com>
2017-08-01 16:02:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+module.exports = function notifyCap(dispatch) {
+    const chat = new (require('../../cat_utils/chatCommands.js'))(dispatch)
+    const msg = new (require('../../cat_utils/message.js'))(dispatch, 'notify', '#ffffff')
+    const inv = new (require('../../cat_utils/inventory.js'))(dispatch, notifyInventory)
+    const ploc = new (require('../../cat_utils/playerLocation.js'))(dispatch)
+    // chat.add('notify test', test)
+    // chat.add('notify stop', stop)
+
+    var vgCooldown = false
+    var vgTimer = null
+    var markCooldown = false
+    var markTimer = null
+
+    var timer = null
+    var style = 0
+
+    function notifyInventory() {
+        if (!markCooldown) {
+            var marks = inv.getAmount(151643)
+            if (marks > 70) {
+                markCooldown = true
+                msg.notify('WARNING: Elleon Marks almost capped (' + marks + '/100)', 44)
+                markTimer = setTimeout(function () { markCooldown = false }, 20000)
+            }
+        }
+    }
+
+    dispatch.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 2, event => {
+        if (!vgCooldown && (event.lastProgress == -1 || event.lastProgress == 1) && event.vgCredits > 7000) {
+            vgCooldown = true
+            msg.notify('WARNING: VG credits almost capped (' + event.vgCredits + '/9000)', 44)
+            vgTimer = setTimeout(function () { vgCooldown = false }, 20000)
+        }
+    });
+
+    // dispatch.hook('S_DUNGEON_GUIDE_ON', 1, event => {
+    //     console.log("cancel guide")
+    //     return false
+    // })
+
+    // // dispatch.hook('S_DUNGEON_UI_HIGHLIGHT', 1, event => {
+    // //     console.log("cancel ui")
+    // //     return false
+    // // })
+
+    // dispatch.hook('S_DUNGEON_VEHICLE_REPAIRABLE', 1, event => {
+    //     return false
+    // })
+
+    // dispatch.hook('S_CAN_DUNGEON_RETRY', 1, event => {
+    //     return false
+    // })
+
+    // function test() {
+    //     msg.notify('test: ' + style, style)
+    //     style++
+    //     timer = setTimeout(test, 1000)
+    // }
+    // function stop() {
+    //     clearTimeout(timer)
+    //     style = 0
+    // }
+
+
+
+
+
+}
